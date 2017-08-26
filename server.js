8c3b7f07e697a0fd4fc9e2dbc498857e4f7be25a
Menelik Tefera <menelikworku@gmail.com>
2017-08-25 23:02:22 -0400

    fixing indentations

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-// Pull in required dependencies
+// *************************************************************
+// *** Dependencies
+// =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
